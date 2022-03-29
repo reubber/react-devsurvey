23330eb8570165df750a136baed47c51f9118388
reubber de sousa <reubbers@gmail.com>
2022-03-29 14:58:19 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -4,9 +4,15 @@ import { Login } from './login'
 
 // garantir que os components nao sejam renderizados no inicio.
 describe('Login Component', () => {
-  test('Should not render spinner and error on start', () => {
+  test('Should start with inital state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
   })
 })
