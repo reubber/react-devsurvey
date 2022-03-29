7cfd19c34c8e0678cc18979d559cb6bc02dbeb84
reubber de sousa <reubbers@gmail.com>
2022-03-29 13:52:22 -0300

    refactor: move components to login file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import { Spinner, LoginHeader, Footer } from '@/presentation/components'
+import { Spinner, LoginHeader, Footer, InputBase, FormStatusBase } from '@/presentation/components'
 
 const LoginCmp: React.FC = () => {
   return (
@@ -8,20 +8,14 @@ const LoginCmp: React.FC = () => {
       <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder='Digite seu e-mail' />
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input type="password" name="password" placeholder='Digite sua senha' />
-          <span className={Styles.status}>🔴</span>
-        </div>
+        <InputBase type='email' name='email' placeholder='Digite seu e-mail' state={undefined} setState={undefined} />
+        <InputBase type='password' name='password' placeholder='Digite sua senha' state={undefined} setState={undefined} />
         <button className={Styles.submit} type='submit'>Entrar</button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
+        <FormStatusBase state={{ isLoading: false }} >
           <Spinner className={Styles.spinner} />
           <span className={Styles.error}>Erro</span>
-        </div>
+        </FormStatusBase>
       </form>
       <Footer />
     </div>
