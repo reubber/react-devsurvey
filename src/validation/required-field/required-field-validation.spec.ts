609d5ca83042a50842c6e55fe60f7b3f552e0186
reubber de sousa <reubbers@gmail.com>
2022-03-30 17:15:48 -0300

    test: ensure RequiredFieldValidation return false if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { RequiredFieldError } from '../erros'
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
-  test('Should return erro if field is empty', () => {
+  test('Should return error if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is notempty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
