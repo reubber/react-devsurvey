4670f32b8f8d2a957ef3b32e77343ff656424836
reubber de sousa <reubbers@gmail.com>
2022-03-31 10:44:00 -0300

    test: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.spec.ts
@@ -0,0 +1,11 @@
+import { InvalidFieldError } from '@/validation/erros'
+
+import { EmailValidation } from './email-validation'
+
+describe('EmailValidation', () => {
+  it('Should return error if email is invalid', () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate('')
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
